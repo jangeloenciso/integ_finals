25dc46271ea082592dad182912069ac65cc24dbf
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-05-27 18:02:40 +0800

    more fix

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ include_once('functions/functions.php')
 <h1>Login</h1>
 <form action="routes/homepage.php" method="post">
     <input type="text" name="username" placeholder="Username" required>
+    <br>
+    <input type="password" name="password" placeholder="Password" required>
+    <br>
     <input type="submit" value="Submit">    
 </form>
 
