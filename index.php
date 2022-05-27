14ab778fdb33cda2addcc6f268ac77f4359f9b48
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-05-27 16:55:52 +0800

    Required username

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once('templates/header.php');
 ?>
 <h1>Login</h1>
 <form action="routes/homepage.php" method="post">
-    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="username" placeholder="Username" required>
     <input type="submit" value="Submit">    
 </form>
 
